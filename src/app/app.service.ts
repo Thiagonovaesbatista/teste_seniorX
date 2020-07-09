25624503ff84ed9bb9e4fe93b94f10422e1e8872
Thiagonovaesbatista <thiago.novaes.batista@gmail.com>
2020-07-09 15:17:01 -0300

    commit

---
+++ src/app/app.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+//componentes que obtém os dados de autenticação da plataforma
+import { service, user } from '@seniorsistemas/platform-data';
+import { forkJoin, Observable, from } from 'rxjs';
+import { mergeMap} from 'rxjs/operators';
+
+@Injectable()
+export class AppService {
+  constructor(private http: HttpClient) { }
+
+  getDadosUsuario(): Observable<any> {
+    return forkJoin(
+      //busca a url base e o token para montagem do request.
+      from(service.getRestUrl()),
+      from(user.getAuthHeader()),
+    ).pipe(mergeMap((values: any) => {
+      const [bridgeUrl, authHeader] = values;
+      let headers = new HttpHeaders({
+        "Authorization": authHeader
+      });
+      return this.http.get<any>(`${bridgeUrl}usuarios/userManager/queries/obterMeusDados`, { headers });;
+    }));
+  }
+}
